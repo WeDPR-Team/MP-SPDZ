3b356c3df4961a6ed63175d6db1b8fe7575313b0
Shareong <740310627@qq.com>
2022-01-06 16:04:42 +0800

    detach task data

--- Processor/Processor.hpp
+++ Processor/Processor.hpp
@@ -3,6 +3,7 @@
 
 #include "Processor/Processor.h"
 #include "Processor/Program.h"
+#include "Processor/PpcConstant.h"
 #include "GC/square64.h"
 
 #include "Protocols/ReplicatedInput.hpp"
@@ -374,7 +375,8 @@ void Processor<sint, sgf2n>::read_socket_private(int client_id,
 template<class sint, class sgf2n>
 void Processor<sint, sgf2n>::read_shares_from_file(int start_file_posn, int end_file_pos_register, const vector<int>& data_registers) {
   string filename;
-  filename = "Data-Shares/Transactions-P" + to_string(P.my_num()) + ".data";
+  // filename = "Data-Shares/Transactions-P" + to_string(P.my_num()) + ".data";
+  filename = "Data-Shares/" + get_job_id() +  "-" + to_string(P.my_num()) + ".data";
 
   unsigned int size = data_registers.size();
 
@@ -416,26 +418,6 @@ void Processor<sint, sgf2n>::write_shares_to_file(const vector<int>& data_regist
 
   binary_file_io.write_to_file(filename, inpbuf);
 }
-
-// template<class sint, class sgf2n>
-// void Processor<sint, sgf2n>::write_shares_to_file(const vector<int>& data_registers) {
-//   string dir = "Persistence";
-//   mkdir_p(dir.c_str());
-
-//   string filename;
-//   filename = dir + "/Transactions-P" + to_string(P.my_num()) + ".data";
-
-//   unsigned int size = data_registers.size();
-
-//   vector< sint > inpbuf (size);
-
-//   for (unsigned int i = 0; i < size; i++)
-//   {
-//     inpbuf[i] = get_Sp_ref(data_registers[i]);
-//   }
-
-//   binary_file_io.write_to_file(filename, inpbuf);
-// }
 
 template <class T>
 void SubProcessor<T>::POpen(const vector<int>& reg,const Player& P,int size)
